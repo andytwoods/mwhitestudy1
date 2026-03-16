86d539c01ef5aa0476b654720f6606f3f83aafb3
Andy Woods <andytwoods@users.noreply.github.com>
2026-03-16 12:52:07 +0000

    Update production settings to include Appliku host and configure CSRF trusted origins

--- config/settings/production.py
+++ config/settings/production.py
@@ -9,7 +9,8 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com", "mwhitestudy1.applikuapp.com"])
+CSRF_TRUSTED_ORIGINS = [f"https://{host}" for host in ALLOWED_HOSTS if host not in ("example.com",)]
 
 # DATABASES
 # ------------------------------------------------------------------------------
